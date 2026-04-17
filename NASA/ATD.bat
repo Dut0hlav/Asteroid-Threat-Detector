959b0709e40cefd0d2c5e75cc39e49b578834635
Dut0hlav <129541421+Dut0hlav@users.noreply.github.com>
2026-04-17 03:41:37 +0200

    Nahrání projektu

---
+++ NASA/ATD.bat
@@ -0,0 +1,26 @@
+@echo off
+chcp 65001 >nul
+echo =============================================================
+echo   System pro analyzu a predikci nebezpecnych asteroidu (ATA)
+echo =============================================================
+echo.
+
+echo Krok 1: Kontroluji a vytvarim izolovanou bublinu (venv)...
+echo (Ochrana pred nutnosti mit administratorska prava)
+if not exist "venv\" (
+    python -m venv venv
+)
+
+echo.
+echo Krok 2: Aktivuji bublinu a overuji potrebne knihovny...
+call venv\Scripts\activate
+python -m pip install --upgrade pip >nul
+pip install -r requirements.txt
+
+echo.
+echo Krok 3: Spoustim aplikaci...
+python UI\Aplikace.py
+
+echo.
+echo Aplikace byla ukoncena.
+pause
